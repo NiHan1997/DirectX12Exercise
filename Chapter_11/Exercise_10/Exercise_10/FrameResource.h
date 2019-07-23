9aa79a41f106d86c1fdb76a4af92b33744691a5c
NiHanGame <371429357@qq.com>
2019-07-23 16:07:29 +0800

    23 Chapter 11 Exercise_10

---
+++ Chapter_11/Exercise_10/Exercise_10/FrameResource.h
@@ -0,0 +1,72 @@
+#pragma once
+
+#include "..\..\..\Common\d3dUtil.h"
+#include "..\..\..\Common\MathHelper.h"
+#include "..\..\..\Common\UploadBuffer.h"
+
+using namespace DirectX;
+
+struct Vertex
+{
+	Vertex() = default;
+	Vertex(float x, float y, float z, float nx, float ny, float nz, float u, float v) :
+		Position(x, y, z), Normal(nx, ny, nz), TexC(u, v) 
+	{
+	}
+
+	XMFLOAT3 Position;
+	XMFLOAT3 Normal;
+	XMFLOAT2 TexC;
+};
+
+struct ObjectConstants
+{
+	XMFLOAT4X4 World = MathHelper::Identity4x4();
+	XMFLOAT4X4 TexTransform = MathHelper::Identity4x4();
+};
+
+struct PassConstants
+{
+	DirectX::XMFLOAT4X4 View = MathHelper::Identity4x4();
+	DirectX::XMFLOAT4X4 InvView = MathHelper::Identity4x4();
+	DirectX::XMFLOAT4X4 Proj = MathHelper::Identity4x4();
+	DirectX::XMFLOAT4X4 InvProj = MathHelper::Identity4x4();
+	DirectX::XMFLOAT4X4 ViewProj = MathHelper::Identity4x4();
+	DirectX::XMFLOAT4X4 InvViewProj = MathHelper::Identity4x4();
+
+	DirectX::XMFLOAT3 EyePosW = { 0.0f, 0.0f, 0.0f };
+	float cbPerObjectPad1 = 0.0f;
+	DirectX::XMFLOAT2 RenderTargetSize = { 0.0f, 0.0f };
+	DirectX::XMFLOAT2 InvRenderTargetSize = { 0.0f, 0.0f };
+
+	float NearZ = 0.0f;
+	float FarZ = 0.0f;
+	float TotalTime = 0.0f;
+	float DeltaTime = 0.0f;
+
+	XMFLOAT4 AmbientLight = { 0.0f, 0.0f, 0.0f, 1.0f };
+	XMFLOAT4 FogColor = { 0.7f, 0.7f, 0.7f, 1.0f };
+	float gFogStart = 5.0f;
+	float gFogRange = 150.0f;
+	DirectX::XMFLOAT2 cbPerObjectPad2 = { 0.0f, 0.0f };
+	Light Lights[MaxLights];
+};
+
+
+class FrameResource
+{
+public:
+	FrameResource(ID3D12Device* device, UINT passCount, UINT objCount, UINT materialCount);
+	FrameResource(const FrameResource& rhs) = delete;
+	FrameResource& operator=(const FrameResource& rhs) = delete;
+	~FrameResource();
+
+	Microsoft::WRL::ComPtr<ID3D12CommandAllocator> CmdAlloc = nullptr;
+
+	std::unique_ptr<UploadBuffer<PassConstants>> PassCB = nullptr;
+	std::unique_ptr<UploadBuffer<ObjectConstants>> ObjectCB = nullptr;
+	std::unique_ptr<UploadBuffer<MaterialConstants>> MaterialCB = nullptr;
+
+	UINT64 Fence = 0;
+};
+
