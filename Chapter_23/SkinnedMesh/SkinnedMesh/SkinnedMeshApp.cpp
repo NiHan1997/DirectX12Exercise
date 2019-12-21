7cf4a6bb3c2d34c371de8946746d066229caf3da
NiHanGame <371429357@qq.com>
2019-12-21 16:32:39 +0800

    Fix SDK18362 can't convert "bool" to "LPCWSTR".

--- Chapter_23/SkinnedMesh/SkinnedMesh/SkinnedMeshApp.cpp
+++ Chapter_23/SkinnedMesh/SkinnedMesh/SkinnedMeshApp.cpp
@@ -90,7 +90,7 @@ void SkinnedMeshApp::Update(const GameTimer& gt)
 
 	if (mCurrFrameResource->Fence != 0 && mFence->GetCompletedValue() < mCurrFrameResource->Fence)
 	{
-		HANDLE eventHandle = CreateEventEx(nullptr, false, false, EVENT_ALL_ACCESS);
+		HANDLE eventHandle = CreateEventEx(nullptr, nullptr, false, EVENT_ALL_ACCESS);
 		ThrowIfFailed(mFence->SetEventOnCompletion(mCurrFrameResource->Fence, eventHandle));
 		WaitForSingleObject(eventHandle, INFINITE);
 		CloseHandle(eventHandle);
